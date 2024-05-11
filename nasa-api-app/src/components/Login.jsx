74741c0eb30f83118ad66b06708f42558aaae4a3
Ishani Abeysinghe <99570856+it21333552@users.noreply.github.com>
2024-05-11 09:59:45 +0530

    Update Login.jsx

--- nasa-api-app/src/components/Login.jsx
+++ nasa-api-app/src/components/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const response = await axios.post(
-        "https://nasa-app-server-production.up.railway.app/api/users/login",
+        "nasa-app-backend-production.up.railway.app/api/users/login",
         user
       );
       const token = response.data.token;
